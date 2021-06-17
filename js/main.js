0c9896b1dfeb55cb18a264651b18cd8f26337591
Anna <samanna@ngs.ru>
2021-06-17 16:52:22 +0700

    module4 task1

--- js/main.js
+++ js/main.js
@@ -1,37 +1,74 @@
-/* link to source https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random */
+const ADVERTISEMENT_COUNT=10;
+const TYPES =[
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel',
+];
+const CHECKINS =[
+  '12:00',
+  '13:00',
+  '14:00',
+];
+const CHECKOUTS =[
+  '12:00',
+  '13:00',
+  '14:00',
+];
+const FEATURES =[
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+const PHOTOS =[
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+const MIN=1;
+const MAX=1000;
+const latMIN=35.65000;
+const latMAX=35.70000;
+const LAT_LNG_PRECISION=5;
+const lngMIN=139.70000;
+const lngMAX=139.80000;
 
-function getRandomIntegerInclusive (min, max)
-{
-  let swap=0;
+const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0,elements.length-1)];
 
-  if (max<min)
-  {
-    swap=min;
-    min=max;
-    max=swap;
-  }
+const avatarNumber = () =>{
+  const index=getRandomPositiveInteger(1,10);
+  // eslint-disable-next-line prefer-template
+  return (index===10)? index.toString: '0'+index;
+};
 
-  min=Math.ceil(min);
-  max=Math.floor(max);
-  return Math.floor(Math.random()*(max-min+1))+min;
-}
+const createAdvertisementObject = () => {
+  return {
+    author: {
+      avatar: 'img/avatars/user'+avatarNumber+'.png',
+    },
+    offer: {
+      title: 'Сдается в аренду.',
+      address: location.lat.toString + location.lng.toString,
+      price: getRandomPositiveInteger(MIN, MAX),
+      type: getRandomArrayElement(TYPES),
+      rooms: getRandomPositiveInteger(MIN, MAX),
+      guests: getRandomPositiveInteger(MIN, MAX),
+      checkin: getRandomArrayElement(CHECKINS),
+      checkout: getRandomArrayElement(CHECKOUTS),
+      features: new Array(getRandomPositiveInteger(0,FEATURES.length-1)).fill(null).map(getRandomArrayElement(FEATURES))
+      ,
+      description: 'Есть все необходимое для полноценного проживания!',
+      photos: new Array(getRandomPositiveInteger(0,PHOTOS.length-1)).fill(null).map(getRandomArrayElement(PHOTOS)),
+    },
+    location: {
+      lat: getRandomPositiveFloat (latMIN, latMAX, digits = LAT_LNG_PRECISION),
+      lng: getRandomPositiveFloat (lngMIN, lngMAX, digits = LAT_LNG_PRECISION),
+    },
+  };
+};
 
-function getRandomFloat(minInclusive, maxInclusive, precision)
-{
-  let swap=0;
-
-  if (maxInclusive<minInclusive)
-  {
-    swap=minInclusive;
-    minInclusive=maxInclusive;
-    maxInclusive=swap;
-  }
-
-  maxInclusive = Math.ceil(maxInclusive/(1/Math.pow(10, precision)));
-  minInclusive = Math.floor(minInclusive/(1/Math.pow(10, precision)));
-  const randomNumber = Math.floor(Math.random()*(maxInclusive - minInclusive + 1)) + minInclusive;
-  return randomNumber / Math.pow(10, precision);
-}
-
-getRandomIntegerInclusive(1,100);
-getRandomFloat(1.1, 1.9, 5);
+const getArrayofAdvertisementObjects = new Array (ADVERTISEMENT_COUNT).fill(null).map(()=>createAdvertisementObject());
