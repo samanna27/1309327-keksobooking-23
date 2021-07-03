2eca0382f069939853daea167ff6441c2f521558
Anna <samanna@ngs.ru>
2021-07-03 22:04:43 +0700

    nodule7-task2-renamed-variables

--- js/form-processing.js
+++ js/form-processing.js
@@ -1,36 +1,36 @@
 const inactivateForm = () => {
   const adFormElement = document.querySelector('.ad-form');
   adFormElement.classList.add('ad-form--disabled');
-  const formInteractiveElements = adFormElement.querySelectorAll('fieldset');
-  formInteractiveElements.forEach ((item) => {
+  const adFormFieldsetElements = adFormElement.querySelectorAll('fieldset');
+  adFormFieldsetElements.forEach ((item) => {
     item.setAttribute('disabled', 'disabled');
   });
 
-  const mapElement = document.querySelector('.map__filters');
-  mapElement.classList.add('map__filters--disabled');
-  const mapInteractiveElements = mapElement.querySelectorAll('.map__filter');
-  mapInteractiveElements.forEach ((item) => {
+  const mapFiletrsElement = document.querySelector('.map__filters');
+  mapFiletrsElement.classList.add('map__filters--disabled');
+  const mapFilterInteractiveElements = mapFiletrsElement.querySelectorAll('.map__filter');
+  mapFilterInteractiveElements.forEach ((item) => {
     item.setAttribute('disabled', 'disabled');
   });
-  const mapFieldsetElement = mapElement.querySelector('fieldset');
+  const mapFieldsetElement = mapFiletrsElement.querySelector('fieldset');
   mapFieldsetElement.setAttribute('disabled', 'disabled');
 };
 
 const activateForm = () => {
   const adFormElement = document.querySelector('.ad-form');
   adFormElement.classList.remove('ad-form--disabled');
-  const formInteractiveElements = adFormElement.querySelectorAll('fieldset');
-  formInteractiveElements.forEach ((item) => {
+  const adFormFieldsetElements = adFormElement.querySelectorAll('fieldset');
+  adFormFieldsetElements.forEach ((item) => {
     item.removeAttribute('disabled');
   });
 
-  const mapElement = document.querySelector('.map__filters');
-  mapElement.classList.remove('map__filters--disabled');
-  const mapInteractiveElements = mapElement.querySelectorAll('.map__filter');
-  mapInteractiveElements.forEach ((item) => {
+  const mapFiltersElement = document.querySelector('.map__filters');
+  mapFiltersElement.classList.remove('map__filters--disabled');
+  const mapFilterInteractiveElements = mapFiltersElement.querySelectorAll('.map__filter');
+  mapFilterInteractiveElements.forEach ((item) => {
     item.removeAttribute('disabled');
   });
-  const mapFieldsetElement = mapElement.querySelector('fieldset');
+  const mapFieldsetElement = mapFiltersElement.querySelector('fieldset');
   mapFieldsetElement.removeAttribute('disabled');
 };
 
