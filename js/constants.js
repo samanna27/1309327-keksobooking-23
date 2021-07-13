4f419160d0232095c942fd416290b6d3c8fb36a5
Anna <samanna@ngs.ru>
2021-07-13 12:42:06 +0700

    changes in files

--- js/constants.js
+++ js/constants.js
@@ -1,5 +1,4 @@
 const ADVERTISEMENT_COUNT = 10;
-const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
 const TYPESMODIFIER = {
   palace: 'Дворец',
   flat: 'Квратира',
@@ -7,52 +6,34 @@ const TYPESMODIFIER = {
   bungalow: 'Бунгало',
   hotel: 'Отель',
 };
-const CHECKINS = ['12:00', '13:00', '14:00'];
-const CHECKOUTS = ['12:00', '13:00', '14:00'];
-const FEATURES = [
-  'wifi',
-  'dishwasher',
-  'parking',
-  'washer',
-  'elevator',
-  'conditioner',
-];
-const PHOTOS = [
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
-];
-const MIN = 1;
-const MAX = 1000;
-const LATMIN = 35.65;
-const LATMAX = 35.7;
-const LAT_LNG_PRECISION = 5;
-const LNGMIN = 139.7;
-const LNGMAX = 139.8;
-const MINLENGTH = 1;
-const AVATARMININDEX = 1;
-const AVATARMAXINDEX = 10;
 const MIN_TITLE_LENGTH = 30;
 const MAX_TITLE_LENGTH = 100;
+const TYPE_MIN_PRICE = {
+  bungalow: 0,
+  flat: 1000,
+  hotel: 3000,
+  house: 5000,
+  palace: '10000',
+};
+const TYPE_PLACEHOLDER = {
+  bungalow: '0',
+  flat: '1 000',
+  hotel: '3 000',
+  house: '5 000',
+  palace: '10 000',
+};
+const PIN_DEFAULT_LAT = 35.7;
+const PIN_DEFAULT_LNG = 139.6;
+const ALERT_SHOW_TIME = 5000;
 
 export {
   ADVERTISEMENT_COUNT,
-  TYPES,
   TYPESMODIFIER,
-  CHECKINS,
-  CHECKOUTS,
-  FEATURES,
-  PHOTOS,
-  MIN,
-  MAX,
-  LATMIN,
-  LATMAX,
-  LAT_LNG_PRECISION,
-  LNGMIN,
-  LNGMAX,
-  MINLENGTH,
-  AVATARMININDEX,
-  AVATARMAXINDEX,
   MIN_TITLE_LENGTH,
-  MAX_TITLE_LENGTH
+  MAX_TITLE_LENGTH,
+  TYPE_MIN_PRICE,
+  TYPE_PLACEHOLDER,
+  PIN_DEFAULT_LAT,
+  PIN_DEFAULT_LNG,
+  ALERT_SHOW_TIME
 };
--- js/main.js
+++ js/main.js
@@ -1,7 +1,220 @@
-import { offersList } from './popup-card-creation.js';
-import { inactivateForm, activateForm } from './form-processing.js';
+import {
+  inactivateForm,
+  activateForm,
+  adFormElement
+} from './form-processing.js';
+import { TYPESMODIFIER, PIN_DEFAULT_LAT, PIN_DEFAULT_LNG, ADVERTISEMENT_COUNT } from './constants.js';
+import { getData, sendData } from './api.js';
 
-// eslint-disable-next-line no-console
-console.log(offersList);
+const addressElement = adFormElement.querySelector('#address');
+const resetButtonElement = document.querySelector('.ad-form__reset');
+
 inactivateForm();
-activateForm();
+
+const map = L.map('map-canvas')
+  .on('load', () => {
+    activateForm();
+    addressElement.value = `${PIN_DEFAULT_LAT}, ${PIN_DEFAULT_LNG}`;
+  })
+  .setView(
+    {
+      lat: PIN_DEFAULT_LAT,
+      lng: PIN_DEFAULT_LNG,
+    },
+    10,
+  );
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+const mainPinIcon = L.icon({
+  iconUrl: '/img/main-pin.svg',
+  iconSize: [52, 52],
+  iconAnchor: [26, 52],
+});
+
+const mainPinMarker = L.marker(
+  {
+    lat: PIN_DEFAULT_LAT,
+    lng: PIN_DEFAULT_LNG,
+  },
+  {
+    draggable: true,
+    icon: mainPinIcon,
+  },
+);
+
+mainPinMarker.addTo(map);
+
+mainPinMarker.on('moveend', (evt) => {
+  const coordinates = evt.target.getLatLng();
+  const coordinateLat = coordinates.lat.toFixed(5);
+  const coordinateLng = coordinates.lng.toFixed(5);
+  addressElement.value = `${coordinateLat}, ${coordinateLng}`;
+});
+
+const adPinIcon = L.icon({
+  iconUrl: '/img/pin.svg',
+  iconSize: [40, 40],
+  iconAnchor: [20, 40],
+});
+
+resetButtonElement.addEventListener('click', () => {
+  mainPinMarker.setLatLng({
+    lat: PIN_DEFAULT_LAT,
+    lng: PIN_DEFAULT_LNG,
+  });
+
+  map.setView({
+    lat: PIN_DEFAULT_LAT,
+    lng: PIN_DEFAULT_LNG,
+  },
+  10,
+  );
+});
+
+const createCustomPopup = (offer) => {
+  const cardTemplate = document.querySelector('#card').content;
+  const popupElement = cardTemplate.querySelector('.popup');
+
+  const newPopupElement = popupElement.cloneNode(true);
+
+  const popupTitleElement = newPopupElement.querySelector('.popup__title');
+  if (offer.offer.title) {
+    popupTitleElement.textContent = offer.offer.title;
+  } else {
+    popupTitleElement.remove();
+  }
+
+  const popupAddressElement = newPopupElement.querySelector(
+    '.popup__text--address',
+  );
+  if (offer.offer.address) {
+    popupAddressElement.textContent = offer.offer.address;
+  } else {
+    popupAddressElement.remove();
+  }
+
+  const popupPriceElement = newPopupElement.querySelector(
+    '.popup__text--price',
+  );
+  if (offer.offer.price) {
+    popupPriceElement.textContent = `${offer.offer.price} ₽/ночь`;
+  } else {
+    popupPriceElement.remove();
+  }
+
+  const popupTypeElement = newPopupElement.querySelector('.popup__type');
+  if (offer.offer.type) {
+    popupTypeElement.textContent = TYPESMODIFIER[offer.offer.type];
+  } else {
+    popupTypeElement.remove();
+  }
+
+  const popupCapacityElement = newPopupElement.querySelector(
+    '.popup__text--capacity',
+  );
+  if (offer.offer.crooms || offer.offer.guests) {
+    popupCapacityElement.textContent = `${offer.offer.rooms} комнаты для ${offer.offer.guests} гостей`;
+  } else {
+    popupCapacityElement.remove();
+  }
+
+  const popupTimeElement = newPopupElement.querySelector('.popup__text--time');
+  if (offer.offer.checkin || offer.offer.checkout) {
+    popupTimeElement.textContent = `Заезд после ${offer.offer.checkin}, выезд до ${offer.offer.checkout}`;
+  } else {
+    popupTimeElement.remove();
+  }
+
+  const popupFeaturesElement =
+    newPopupElement.querySelector('.popup__features');
+  if (offer.offer.features) {
+    const featuresModifiers = offer.offer.features.map(
+      (feature) => `popup__feature--${feature}`,
+    );
+    popupFeaturesElement.querySelectorAll('.popup__feature').forEach((item) => {
+      const modifier = item.classList[1];
+      if (!featuresModifiers.includes(modifier)) {
+        item.remove();
+      }
+    });
+  } else {
+    popupFeaturesElement.remove();
+  }
+
+  const popupDescriptionElement = newPopupElement.querySelector(
+    '.popup__description',
+  );
+  if (offer.offer.description) {
+    popupDescriptionElement.textContent = offer.offer.description;
+  } else {
+    popupDescriptionElement.remove();
+  }
+
+  const offerElementPhotoArray = offer.offer.photos;
+  const popupPhotosListElement =
+    newPopupElement.querySelector('.popup__photos');
+  if (offerElementPhotoArray) {
+    offerElementPhotoArray.forEach((item) => {
+      const popupPhotoElementClone = popupPhotosListElement
+        .querySelector('.popup__photo')
+        .cloneNode(true);
+      popupPhotoElementClone.src = item;
+      popupPhotosListElement.appendChild(popupPhotoElementClone);
+    });
+  } else {
+    popupPhotosListElement.remove();
+  }
+  const popupPhotosListElementImagesArray =
+    popupPhotosListElement.querySelectorAll('.popup__photo');
+  popupPhotosListElementImagesArray[0].remove();
+
+  const popupAvatarElement = newPopupElement.querySelector('.popup__avatar');
+  if (offer.author.avatar) {
+    popupAvatarElement.src = offer.author.avatar;
+  } else {
+    popupAvatarElement.remove();
+  }
+
+  return newPopupElement;
+};
+
+const markerGroup = L.layerGroup().addTo(map);
+
+const createMarker = (offer) => {
+  const lat = offer.location.lat;
+  const lng = offer.location.lng;
+
+  const marker = L.marker(
+    {
+      lat,
+      lng,
+    },
+    {
+      icon: adPinIcon,
+    },
+  );
+
+  marker.addTo(markerGroup).bindPopup(createCustomPopup(offer), {
+    keepInView: true,
+  });
+};
+
+getData((offers) => {
+  offers
+    .slice(0,ADVERTISEMENT_COUNT)
+    .forEach((item)=>{
+      createMarker(item);
+    });
+});
+
+adFormElement.addEventListener('submit', (evt)=>{
+  evt.preventDefault();
+  const formData = new FormData(evt.target);
+  sendData(formData);
+});
+
+export { map, mainPinMarker };
